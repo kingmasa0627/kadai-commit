b4b258d83ab8029269997192196fb88e729977b0
Masanari.Kashima <m.life2729@gmail.com>
2019-11-06 02:57:34 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+
+        <title>おみくじプログラム</title>
+    </head>
+    <body>
+        <h1>おみくじプログラム</h1>
+        
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <button type="submit" name="btn" class="btn btn-primary" value="draw">おみくじを引く</button>
+        </form>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
